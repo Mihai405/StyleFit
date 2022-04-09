f177b7945cffc4406b2103390999af250cc1a5cb
Mihai405 <mihaijurj2000@yahoo.com>
2022-04-09 21:35:57 +0300

    Delete unused files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,8 @@
 import logo from "./logo.png";
-import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>StyleFit App</p>
-            </header>
-        </div>
+        <h1>StyleFit App</h1>
     );
 }
 
