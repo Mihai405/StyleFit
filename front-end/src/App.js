c85230ec85d66f6a51dcbfaa1b2c635dce7dd239
sebastianiscruleasa <sebi.iscruleasa@yahoo.ro>
2022-05-05 16:09:05 +0300

    restructured the project

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./Layout/NavBar";
+import { Fragment } from "react";
+
+function App() {
+    return (
+        <Fragment>
+            <NavBar />
+        </Fragment>
+    );
+}
+
+export default App;
