b998ec1870779cb26ed8f3f41162152718bfa22d
sebastianiscruleasa <sebi.iscruleasa@yahoo.ro>
2022-05-22 15:23:34 +0300

    mapped dummy data in partnerPage

--- front-end/src/Pages/PartnerPage.js
+++ front-end/src/Pages/PartnerPage.js
@@ -1,7 +1,29 @@
+import { Grid } from "@mui/material";
+
 import PriceCard from "../components/PartnerPage/PriceCard";
 
+const dummy_data = [
+  { id: 1, price: 100, name: "Therapeutic Massage" },
+  { id: 2, price: 100, name: "Therapeutic Massage1" },
+  { id: 3, price: 100, name: "Therapeutic Massage2" },
+  { id: 4, price: 100, name: "Therapeutic Massage3" },
+  { id: 5, price: 100, name: "Therapeutic Massage4" },
+  { id: 6, price: 100, name: "Therapeutic Massage5" },
+];
+
 const PartnerPage = () => {
-  return <PriceCard />;
+  return (
+    <Grid container>
+      <Grid item md={4}></Grid>
+      <Grid container item md={8} spacing={3}>
+        {dummy_data.map((element, index) => (
+          <Grid item md={4} key={index}>
+            <PriceCard {...element} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default PartnerPage;
--- front-end/src/components/PartnerPage/PriceCard.js
+++ front-end/src/components/PartnerPage/PriceCard.js
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 
-const PriceCard = () => {
+const PriceCard = ({ price, name }) => {
   return (
     <Box display="flex" alignItems="center">
       <Box
@@ -9,15 +9,15 @@ const PriceCard = () => {
         alignItems="center"
         sx={{
           border: "1px solid black",
-          borderRadius: "50%",
+          borderRadius: "100%",
           padding: "10px",
           marginRight: "10px",
         }}
       >
-        <Typography>100</Typography>
+        <Typography>{price}</Typography>
         <Typography>RON</Typography>
       </Box>
-      <Typography component="span">Therapeutic Massage</Typography>
+      <Typography component="span">{name}</Typography>
     </Box>
   );
 };
