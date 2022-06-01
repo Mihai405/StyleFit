e44f93bbab7828106a511f323ccbbf42a10ccf57
sebastianiscruleasa <sebi.iscruleasa@yahoo.ro>
2022-06-01 19:25:36 +0300

    auth-context added

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,12 +5,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { AuthContextProvider } from "./store/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <AuthContextProvider>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
