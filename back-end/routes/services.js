6e40769f48cf9436ff1de91ea074bfbedf42a0d3
Mihai405 <mihaijurj2000@yahoo.com>
2022-06-01 18:35:43 +0300

    get all services

--- back-end/routes/services.js
+++ back-end/routes/services.js
@@ -4,6 +4,15 @@ const Appointment = require("../models/appointment");
 const router = express.Router();
 const Service = require("../models/service");
 
+router.get("/", async (req, res) => {
+  try {
+    const services = await Service.find();
+    res.json(services);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 router.post("/:id/appointments", auth, async (req, res) => {
   const service = await Service.findById(req.params.id);
   try {
